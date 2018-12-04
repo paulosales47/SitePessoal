b61c6ea88834955c3bca61a61d12cf2769ea3949
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2018-12-03 22:03:31 -0200

    Criando componente Formação Academica

---
+++ src/app/formacao-academica/formacao-academica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formacao-academica',
+  templateUrl: './formacao-academica.component.html',
+  styleUrls: ['./formacao-academica.component.css']
+})
+export class FormacaoAcademicaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
