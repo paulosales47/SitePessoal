27c0a26e509f3bc79f1da70003bfb0cf61da552f
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2018-12-03 23:36:44 -0200

    Configurando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormacaoAcademicaComponent } from './formacao-academica/formacao-academica.component';
+import { ExperienciaProfissionalComponent } from './experiencia-profissional/experiencia-profissional.component';
+import { ResumoComponent } from './resumo/resumo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path:'', component: ResumoComponent}
+  ,{path:'FormacaoAcademica', component: FormacaoAcademicaComponent}
+  ,{path:'ExperienciaProfissional', component: ExperienciaProfissionalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
