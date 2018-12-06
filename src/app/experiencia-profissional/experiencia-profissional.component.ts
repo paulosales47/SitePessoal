c1142149fe237ed733fb4a106acaa314e0aa43ea
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2018-12-05 22:48:38 -0200

    Configurando animação sequencial

--- src/app/experiencia-profissional/experiencia-profissional.component.ts
+++ src/app/experiencia-profissional/experiencia-profissional.component.ts
@@ -11,24 +11,24 @@ import { trigger, style, state, transition, animate, keyframes } from '@angular/
       state('criado', style({'opacity': 1})),
       transition('void => criado', [style({'opacity': 0, transform: 'translate(-100px, 0px)'}), animate('1500ms ease-in-out')])
     ])
-    // ,
-    // trigger('animacao-experiencia-2',
-    // [
-    //   state('criado', style({'opacity': 1})),
-    //   transition('void => criado', [style({'opacity': 0, transform: 'translate(100px, 0px)'}), animate('1500ms ease-in-out')])
-    // ])
-    // ,
-    // trigger('animacao-experiencia-3',
-    // [
-    //   state('criado', style({'opacity': 1})),
-    //   transition('void => criado', [style({'opacity': 0, transform: 'translate(-100px, 0px)'}), animate('1500ms ease-in-out')])
-    // ])
-    // ,
-    // trigger('animacao-experiencia-4',
-    // [
-    //   state('criado', style({'opacity': 1})),
-    //   transition('void => criado', [style({'opacity': 0, transform: 'translate(100px, 0px)'}), animate('1500ms ease-in-out')])
-    // ])
+    ,
+    trigger('animacao-experiencia-2',
+    [
+      state('criado', style({'opacity': 1})),
+      transition('inicio => criado', [style({'opacity': 0, transform: 'translate(100px, 0px)'}), animate('1500ms ease-in-out')])
+    ])
+    ,
+    trigger('animacao-experiencia-3',
+    [
+      state('criado', style({'opacity': 1})),
+      transition('inicio => criado', [style({'opacity': 0, transform: 'translate(-100px, 0px)'}), animate('1500ms ease-in-out')])
+    ])
+    ,
+    trigger('animacao-experiencia-4',
+    [
+      state('criado', style({'opacity': 1})),
+      transition('inicio => criado', [style({'opacity': 0, transform: 'translate(100px, 0px)'}), animate('1500ms ease-in-out')])
+    ])
   ]
 })
 export class ExperienciaProfissionalComponent implements OnInit {
@@ -39,12 +39,25 @@ export class ExperienciaProfissionalComponent implements OnInit {
   public estadoExperiencia4: string
   
   constructor() {
-    
-   }
+  }
 
   ngOnInit() {
-
-
-    
+    this.estadoExperiencia2 = 'inicio'
+    this.estadoExperiencia1 = 'criado';
   }
+
+  FimAnimacaoExperiencia1(): void {
+    this.estadoExperiencia3 = 'inicio'
+    this.estadoExperiencia2 = 'criado';
+  }
+
+  FimAnimacaoExperiencia2(): void {
+    this.estadoExperiencia4 = 'inicio'
+    this.estadoExperiencia3 = 'criado'
+  }
+
+  FimAnimacaoExperiencia3(): void {
+    this.estadoExperiencia4 = 'criado'
+  }
+
 }
--- src/app/resumo/resumo.component.ts
+++ src/app/resumo/resumo.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, state, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-resumo',
   templateUrl: './resumo.component.html',
-  styleUrls: ['./resumo.component.css']
+  styleUrls: ['./resumo.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({height: '200px', opacity: 1, backgroundColor: 'yellow' })),
+      state('closed', style({ height: '100px', opacity: 0.5, backgroundColor: 'green' })),
+      transition('open => closed', [ animate('1s') ]),
+      transition('closed => open', [ animate('0.5s')]),
+    ]),
+  ],
 })
 export class ResumoComponent implements OnInit {
+  isOpen = true;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
 
 }
